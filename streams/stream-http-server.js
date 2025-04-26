b35efa76f08a7a25dd3d4d6b02304829fa2a90c7
Eduardo Ferraz <edupferraz@gmail.com>
2025-04-26 07:14:42 -0300

    feat: implement consuming complete streams

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,3 +1,4 @@
+
 import http from 'node:http'
 import { Transform } from 'node:stream'
 
@@ -14,10 +15,18 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
-  return req
-    .pipe(new InverseNumberStream())
-    .pipe(res)
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
 })
 
 server.listen(3334)
